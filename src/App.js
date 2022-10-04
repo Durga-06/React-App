9cd2e1cdaec8b9d05075e9610d820721b649f5ad
Durga Prasad <durgaprasad.thoutam@gmail.com>
2022-10-05 00:13:58 +0100

    Committing changes related to state hook with different examples.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 
 import './App.css';
-
+import {useState} from "react";
 function App() {
+//const age =0;
 
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+    const [age, setAge] = useState(0);
+
+    const [inputValue,setInputValue] = useState("");
+    const [showHideText,setShowHideText] = useState(false);
+    const [colorChange, setColorChange] = useState("red");
+    const handleInputChange = (event) =>{setInputValue(event.target.value); }
+    const handleShowHide = ()=>{ setShowHideText(!showHideText)}
+    const handleColorChange = () => {setColorChange(colorChange === "green"?"red":"green");}
+
+    const decreaseAge = () => {
+        setAge(age-1);
+    }
+    const setToZero = () => {
+        setAge(0);
+    }
   return (
     <div className="App">
-      {planets.map((planet,key)=>planet.isGasPlanet && <h1>{planet.name}</h1>)}
+      {age}
+        <br/>
+      <button onClick={()=>setAge(age+1)}>Increase Age</button>
+<br/><br/>
+        <button onClick={decreaseAge}>Decrease Age</button>
+<br/><br/>
+        <button onClick={setToZero}>Set Age to Zero</button>
+<br/><br/>
+        <input type="text" onChange={handleInputChange}></input>
+        <br/>
+        <p>{inputValue}</p>
+<br/>
+        <button onClick={handleShowHide}>Show/Hide</button>
+        { showHideText && <h1>Hi my name is Durga</h1>}
+<br/>
+        <br/>
+        <button onClick={handleColorChange}>Toggle Color</button>
+        { <h1 style={{color:colorChange}}>Hi my name is Durga</h1>}
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+
+
+
+
+
     </div>
   );
 }
